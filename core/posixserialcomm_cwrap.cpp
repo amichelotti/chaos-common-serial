6905e8653261c66816aa2a961342d6551dd79232
amichelo <andrea.michelotti@lnf.infn.it>
2021-05-04 17:56:25 +0200

    serial error

--- core/posixserialcomm_cwrap.cpp
+++ core/posixserialcomm_cwrap.cpp
@@ -1,7 +1,7 @@
 #include <common/serial/pserial.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #ifdef POSIX_SERIAL_COMM_CWRAP_DEBUG
 #define DEBUG
 #endif
@@ -10,8 +10,8 @@
 #define MAX_HANDLE 100
 extern "C" {
 
-  static common::serial::AbstractSerialChannel* id2handle[MAX_HANDLE]={0};
-  /*
+static common::serial::AbstractSerialChannel* id2handle[MAX_HANDLE] = {0};
+/*
   static int getFirstFreeHandle(){
     int cnt=0;
     for(cnt=0;cnt<MAX_HANDLE;cnt++){
@@ -22,233 +22,221 @@ extern "C" {
     return -1;
   }
   */
-  pserial_handle_t popen_serial(int internal_buffering,const char*serdev,int baudrate,int parity,int bits,int stop,bool hw){
-    int idx=-1;
-  
-    if(serdev==NULL)
-      return -1;
-    
-    if(serdev){
-      
-      const char *pnt=serdev;
-      const char*start_name=serdev;
-      while(*pnt!=0){
-	if(*pnt=='/')
-	  start_name=pnt;
-	pnt++;
-      }
-      if(start_name==serdev){
-	printf("## bad device name specification %s\n",serdev);
-	return -2;
-      }
-      
-      pnt=start_name;
-      while((!isdigit(*pnt)) && (*pnt!=0)){
-	pnt ++;
-      }
-      if(*pnt==0){
-	printf("## cannot find serial device number\n");
-	return -4;
-      }
-      
-      idx = strtoul(pnt,0,16);
-    } else {
-      	printf("## device name not specified\n");
-	return -5;
+pserial_handle_t popen_serial(int internal_buffering, const char* serdev, int baudrate, int parity, int bits, int stop, bool hw) {
+  int idx = -1;
+
+  if (serdev == NULL)
+    return -1;
+
+  if (serdev) {
+    const char* pnt        = serdev;
+    const char* start_name = serdev;
+    while (*pnt != 0) {
+      if (*pnt == '/')
+        start_name = pnt;
+      pnt++;
+    }
+    if (start_name == serdev) {
+      printf("## bad device name specification %s\n", serdev);
+      return -2;
     }
 
-    if(idx<0 || idx>=MAX_HANDLE){
-      printf("## bad index specification %d\n",idx);
-      return -1;
-      }
-    DPRINT("opening %s index %d\n",serdev,idx);
-
-    if(id2handle[idx]!=0){
-      // close before
-      printf("%% serial resource %d was open, closing\n",idx);
-      delete id2handle[idx];
-      id2handle[idx]=0;
-      }
-
-    common::serial::AbstractSerialChannel*p = new common::serial::PosixSerialComm(serdev,baudrate,parity,bits,stop,hw,internal_buffering,internal_buffering);
-    //    common::serial::AbstractSerialComm *p = new common::serial::PosixSerialCommSimple(serdev,baudrate,parity,bits,stop);
-
-    
-    if(p){
-      if(p->init()!=0){
-	printf("## error during initialization\n");
-	delete p;
-	return -7;
-      } else {
-	id2handle[idx] = p;
-	DPRINT("opening serial \"%d\" = 0x%p\n",idx,p);
-	return idx;
-      }
+    pnt = start_name;
+    while ((!isdigit(*pnt)) && (*pnt != 0)) {
+      pnt++;
     }
-    return idx;
-  }
-
-
-  int pclose_serial(pserial_handle_t _h){
-    int ret=-1;
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
+    if (*pnt == 0) {
+      printf("## cannot find serial device number\n");
       return -4;
     }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-      common::serial::AbstractSerialChannel *p =( common::serial::AbstractSerialChannel *)h;
-      ret = p->deinit();
+
+    idx = strtoul(pnt, 0, 16);
+  } else {
+    printf("## device name not specified\n");
+    return -5;
+  }
+
+  if (idx < 0 || idx >= MAX_HANDLE) {
+    printf("## bad index specification %d\n", idx);
+    return -1;
+  }
+  DPRINT("opening %s index %d\n", serdev, idx);
+
+  if (id2handle[idx] != 0) {
+    // close before
+    printf("%% serial resource %d was open, closing\n", idx);
+    delete id2handle[idx];
+    id2handle[idx] = 0;
+  }
+
+  common::serial::AbstractSerialChannel* p = new common::serial::PosixSerialComm(serdev, baudrate, parity, bits, stop, hw, internal_buffering, internal_buffering);
+  //    common::serial::AbstractSerialComm *p = new common::serial::PosixSerialCommSimple(serdev,baudrate,parity,bits,stop);
+
+  if (p) {
+    int ret;
+    if ((ret=p->init()) != 0) {
+      printf("## error during initialization, ret=%d\n",ret);
       delete p;
-      id2handle[_h] = 0;
       return ret;
-    }
-    return ret;
-  }
-
-  int pwrite_async_serial(pserial_handle_t _h, char*buf,int bsize){
-    int ret=-2;
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return -4;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      ret = p->write_async((void*)buf,bsize);
-    }
-    return ret;
-  }
-
-  int pread_async_serial(pserial_handle_t _h, char*buf,int bsize){
-    int ret=-2;
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return -4;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      ret = p->read_async((void*)buf,bsize);
-    }
-    return ret;
-
-  }
-  int pwrite_serial(pserial_handle_t _h, char*buf,int bsize,int timeo,int*timocc){
-    int ret=-2;
-     if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return -4;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-      DPRINT("buf 0x%p size %d timeo %d\n",buf,bsize,timeo);
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      ret = p->write((void*)buf,bsize,timeo,timocc);
-      DPRINT("done return %d timeo %d\n",ret,*timocc);
-    }
-    return ret;
-
-  }
-  int pread_serial(pserial_handle_t _h, char*buf,int bsize,int timeo,int*timocc){
-    int ret=-2;
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return -4;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    
-    if(h){
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      DPRINT("buf 0x%p size %d timeo %d\n",buf,bsize,timeo);
-      ret = p->read((void*)buf,bsize,timeo,timocc);
-      DPRINT("done return %d timeo %d\n",ret,*timocc);
-    }
-    return ret;
-
-  }
-  int pread_serial_count(pserial_handle_t _h){
-    int ret=0;
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return -4;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      ret = p->byte_available_read();
-    }
-    return ret;
-
-  }
-
-  int pwrite_serial_count(pserial_handle_t _h){
-    int ret=0;
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return -4;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      ret = p->byte_available_write();
-    }
-    return ret;
-
-  }
-  void pwrite_flush_serial(pserial_handle_t _h){
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return ;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-    if(h){
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      p->flush_write();
-    }
-  }
-  void pread_flush_serial(pserial_handle_t _h){
-    if(_h>=MAX_HANDLE || _h<0) {
-      printf("## bad handle\n");
-      return ;
-    }
-    common::serial::AbstractSerialChannel* h = id2handle[_h];
-
-    if(h){
-      common::serial::AbstractSerialChannel*p =( common::serial::AbstractSerialChannel*)h;
-      p->flush_read();
-    }
-
-  }
-
-
-  int LVwrite_serial(pserial_handle_t h,void*buf,int bytes,int timeo,int*timoccur){
-    int ret=-1;
-    DPRINT("WRITE resource %d(0x%p), buf: @x%p size %d timeo %d timeout occur(%d)",h, id2handle[h],buf,bytes,timeo,*timoccur);
-
-    if(timeo>0){
-      ret = pwrite_serial(h, (char*)buf,bytes,timeo,timoccur); 
-    } else if(timeo==0){
-      ret = pwrite_async_serial(h, (char*)buf,bytes); 
     } else {
-      ret = pwrite_serial(h, (char*)buf,bytes,0,timoccur); 
+      id2handle[idx] = p;
+      DPRINT("opening serial \"%d\" = 0x%p\n", idx, p);
+      return idx;
     }
-    return ret;
+  }
+  return idx;
 }
 
-  int LVread_serial(pserial_handle_t h,void*buf,int bytes,int timeo,int*timoccur){
-    int ret=-1;
-    DPRINT(" READ resource %d(0x%p), buf: @x%p size %d timeo %d timeout occur(%d)\n",h,id2handle[h],buf,bytes,timeo,*timoccur);
-    if(timeo>0){
-      ret = pread_serial(h, (char*)buf,bytes,timeo,timoccur); 
-    } else if(timeo==0){
-      ret = pread_async_serial(h, (char*)buf,bytes); 
-    } else {
-      ret = pread_serial(h, (char*)buf,bytes,0,timoccur); 
-    }
+int pclose_serial(pserial_handle_t _h) {
+  int ret = -1;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    ret                                      = p->deinit();
+    delete p;
+    id2handle[_h] = 0;
     return ret;
   }
+  return ret;
+}
 
+int pwrite_async_serial(pserial_handle_t _h, char* buf, int bsize) {
+  int ret = -2;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    ret                                      = p->write_async((void*)buf, bsize);
+  }
+  return ret;
+}
+
+int pread_async_serial(pserial_handle_t _h, char* buf, int bsize) {
+  int ret = -2;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    ret                                      = p->read_async((void*)buf, bsize);
+  }
+  return ret;
+}
+int pwrite_serial(pserial_handle_t _h, char* buf, int bsize, int timeo, int* timocc) {
+  int ret = -2;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    DPRINT("buf 0x%p size %d timeo %d\n", buf, bsize, timeo);
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    ret                                      = p->write((void*)buf, bsize, timeo, timocc);
+    DPRINT("done return %d timeo %d\n", ret, *timocc);
+  }
+  return ret;
+}
+int pread_serial(pserial_handle_t _h, char* buf, int bsize, int timeo, int* timocc) {
+  int ret = -2;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    DPRINT("buf 0x%p size %d timeo %d\n", buf, bsize, timeo);
+    ret = p->read((void*)buf, bsize, timeo, timocc);
+    DPRINT("done return %d timeo %d\n", ret, *timocc);
+  }
+  return ret;
+}
+int pread_serial_count(pserial_handle_t _h) {
+  int ret = 0;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    ret                                      = p->byte_available_read();
+  }
+  return ret;
+}
+
+int pwrite_serial_count(pserial_handle_t _h) {
+  int ret = 0;
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return -4;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    ret                                      = p->byte_available_write();
+  }
+  return ret;
+}
+void pwrite_flush_serial(pserial_handle_t _h) {
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    p->flush_write();
+  }
+}
+void pread_flush_serial(pserial_handle_t _h) {
+  if (_h >= MAX_HANDLE || _h < 0) {
+    printf("## bad handle\n");
+    return;
+  }
+  common::serial::AbstractSerialChannel* h = id2handle[_h];
+
+  if (h) {
+    common::serial::AbstractSerialChannel* p = (common::serial::AbstractSerialChannel*)h;
+    p->flush_read();
+  }
+}
+
+int LVwrite_serial(pserial_handle_t h, void* buf, int bytes, int timeo, int* timoccur) {
+  int ret = -1;
+  DPRINT("WRITE resource %d(0x%p), buf: @x%p size %d timeo %d timeout occur(%d)", h, id2handle[h], buf, bytes, timeo, *timoccur);
+
+  if (timeo > 0) {
+    ret = pwrite_serial(h, (char*)buf, bytes, timeo, timoccur);
+  } else if (timeo == 0) {
+    ret = pwrite_async_serial(h, (char*)buf, bytes);
+  } else {
+    ret = pwrite_serial(h, (char*)buf, bytes, 0, timoccur);
+  }
+  return ret;
+}
+
+int LVread_serial(pserial_handle_t h, void* buf, int bytes, int timeo, int* timoccur) {
+  int ret = -1;
+  DPRINT(" READ resource %d(0x%p), buf: @x%p size %d timeo %d timeout occur(%d)\n", h, id2handle[h], buf, bytes, timeo, *timoccur);
+  if (timeo > 0) {
+    ret = pread_serial(h, (char*)buf, bytes, timeo, timoccur);
+  } else if (timeo == 0) {
+    ret = pread_async_serial(h, (char*)buf, bytes);
+  } else {
+    ret = pread_serial(h, (char*)buf, bytes, 0, timoccur);
+  }
+  return ret;
+}
 }
